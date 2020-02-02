fb67672fd6f2f5630c192321651042bb001919fa
CODE.BIER <59924431+a-biernacki@users.noreply.github.com>
2020-02-02 16:05:51 -0500

    Update js.js
    3rd commit

--- js.js
+++ js.js
@@ -1,6 +1,6 @@
 /* 
  * Arianna Biernacki
- * Student ID: 991270882
+ * Student ID: xxxxxxxxx
  * SYST10199 - Web Programming
  */
 prompt ("Press 1 for ROCK" + "\n" + "Press 2 for PAPER" + "\n" + "Press 3 for SCISSOR");
